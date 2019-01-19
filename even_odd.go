b1fe22998bc9c8f2bda6745e81232815309ebbec
Samiksha Bagmar <36911715+Samiksha29@users.noreply.github.com>
2019-01-19 07:35:58 +0530

    Add files via upload

---
+++ even_odd.go
@@ -0,0 +1,16 @@
+package main
+import "fmt"
+func main(){
+	var n1 int=6
+	var n2 int=5
+	if n1%2==0{	
+		fmt.Println("Even !!!")
+	} else {
+	fmt.Println("Odd !!!")
+	}
+	if n2%2==0{	
+		fmt.Println("Even !!!")
+	} else {
+	fmt.Println("Odd !!!")
+	}
+}
---
+++ print_n_numbers.go
@@ -0,0 +1,15 @@
+package main
+import "fmt"
+func main(){
+	var n int=5
+	var i int
+	for i=0;i<n;i++ {
+		fmt.Println(i)
+	}
+	/*
+	for i:=0;i<n;i++ {
+		fmt.Println(i)
+	}*/
+	}
+
+// var i int and i:=0 are same
